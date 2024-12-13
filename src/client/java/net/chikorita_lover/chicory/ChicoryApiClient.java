9a1e4b9428c5f50a5c31c89a29bff2b5946df0e0
Chikorita Lover <cjgardels@gmail.com>
2024-12-13 16:15:13 -0600

    Initial commit

---
+++ src/client/java/net/chikorita_lover/chicory/ChicoryApiClient.java
@@ -0,0 +1,9 @@
+package net.chikorita_lover.chicory;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ChicoryApiClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+	}
+}
---
+++ src/main/java/net/chikorita_lover/chicory/ChicoryApi.java
@@ -0,0 +1,15 @@
+package net.chikorita_lover.chicory;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ChicoryApi implements ModInitializer {
+	public static final Logger LOGGER = LoggerFactory.getLogger("Chicory API");
+	public static final String NAMESPACE = "chicory";
+
+	@Override
+	public void onInitialize() {
+	}
+}
